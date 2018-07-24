c313b5b3ebc5d7eb19eb7b4aef96861c6c6f7bb6
magda <magdalena.aniol@gmail.com>
2018-07-24 19:42:38 +0200

    modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins = [new HtmlWebpackPlugin({
+	template: 'src/index.html',
+	filename: 'index.html',
+	inject: 'body'
+})];
 
 //webpack.config.js
 module.exports = (env) => {
+	if (env === 'production') {
+		plugins.push(
+			new OptimizeJsPlugin({
+				sourceMap: false
+			})
+		)
+	}
+	const environment = env || 'production';
 	return {
-		entry: (env !== 'production' ? [
+		mode: environment,
+		entry: (env !== 'production') ? [
 			'react-hot-loader/patch',
 			'webpack-dev-server/client?http://localhost:8080',
 			'webpack/hot/only-dev-server',
-			] : []).concat(['./client/index.js']),
+		] : []).concat(['./client/index.js']),
 		output: {
 			filename: './bundle.js',
 			path: path.resolve(__dirname, 'public'),
-		},
+		},	
 		module: {
 			rules: [
 				{
@@ -31,6 +48,7 @@ module.exports = (env) => {
 					]
 				}
 			]
-		}
+		},
+		plugins //in ES6 same as plugins: plugins
 	}
 };
